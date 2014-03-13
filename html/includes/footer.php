c21f365ec9c862c52d5abe8196a1f40cdf6055c9
jecr <sinnerei@gmail.com>
2014-03-13 16:43:31 -0600

    Countdown incluido

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -1,7 +1,22 @@
         <aside class="lateral clearfix">
           <div class="widgets1 clearfix hlpr-mr">
             <div class="cuenta">
-              <iframe src="http://free.timeanddate.com/countdown/i41j74es/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cacfff/cpcfff/pct/tcfff/fs100/szw320/szh135/tacfff/tpcfff/macfff/mpcfff/iso2014-06-12T12:00:00" frameborder="0" width="117" height="48"></iframe>
+              <?php
+                $test = date_create();
+                /*Día - Mes - Año  Hora - Minuto - Segundo*/
+                $esteDia = date('d');
+                $anotherTest = date_create($esteDia.'-03-2014 16:55:00');
+              
+                $f1 = date_format($test,'Y-m-d  H:i:s');
+                $f2 = date_format($anotherTest,'Y-m-d  H:i:s');
+                
+                $purinto = date_diff($test,$anotherTest);
+              ?>
+              <div><span id="day"><?php echo $purinto->format('%a'); ?></span> días</div>
+              <div><span id="hour"><?php echo $purinto->format('%h'); ?></span> horas</div>
+              <div><span id="min"><?php echo $purinto->format('%i'); ?></span> minutos</div>
+              <div><span id="sec"><?php echo $purinto->format('%s'); ?></span> segundos</div>
+              <input id="timer_sign" type="hidden" data-sign="<?php echo $purinto->format('%R'); ?>">
             </div>
             <div class="complemento resultados">
               <h2 class="res-head">Últimos Resultados</h2>
@@ -28,13 +43,7 @@
             </div>
             <div class="complemento audios">
               <h2 class="aud-head">Audios</h2>
-              <ul>
-                <li>Audio 1</li>
-                <li>Audio 2</li>
-                <li>Audio 3</li>
-                <li>Audio 4</li>
-                <li>Audio 5</li>
-              </ul>
+              Aquí los audios :0
             </div>
           </div>
           <div class="complemento foto">
@@ -57,18 +66,9 @@
       </footer>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
-        <script src="js/actions.min.js"></script>
         <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/actions.min.js"></script>
 
-        <!--script src="js/plugins.js"></script>
-        <script src="js/main.js"></script-->
-
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
         <?php
           /*Include para el componente de twitter*/
           include('twitter.php');
@@ -103,5 +103,11 @@
           </script>
         </div>
 
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
     </body>
 </html>
--- javascript/actions.js
+++ javascript/actions.js
@@ -130,3 +130,53 @@ $.getJSON('https://gdata.youtube.com/feeds/api/users/'+elCanalDeYoutubeQueQuiere
   };
 
 });
+
+/**
+    /* =================================
+    /* AQUÍ COMIENZA LA CUENTA REGRESIVA
+    /* =================================
+    */
+var dateTimer;
+var iDay = parseInt($('#day').html());
+var iHour = parseInt($('#hour').html());
+var iMin = parseInt($('#min').html());
+var iSec = parseInt($('#sec').html());
+
+if ($('#timer_sign').data('sign') == '+') {
+  dateTimerInitiate();
+} else {
+  timerEnded();
+  $('#day').html('0');
+  $('#hour').html('0');
+  $('#min').html('0');
+  $('#sec').html('0');
+}
+
+function dateTimerInitiate(){
+  clearInterval(dateTimer);
+  dateTimer =  setInterval (function(){
+    iSec--;
+    if (iSec == 0 && iMin == 0 && iHour == 0 && iDay == 0) {
+        timerEnded();
+    } else if (iSec == 0) {
+      iSec = 59;
+      iMin--;
+      if (iMin == -1) {
+        iMin = 59;
+        iHour--;
+        if (iHour == -1) {
+          iHour = 23;
+          iDay --;
+          $('#day').html(iDay);
+        };
+        $('#hour').html(iHour);
+      };
+      $('#min').html(iMin);
+    }
+    $('#sec').html(iSec);
+  }, 1000);
+};
+
+function timerEnded(){
+  clearInterval(dateTimer);
+}
