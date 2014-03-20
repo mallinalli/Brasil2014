f418f3285bfde625d1c50a531fea69871300a6b4
mallinalli <malli.gabarron@gmail.com>
2014-03-20 12:38:43 -0600

    calendario avances

--- html/calendario.php
+++ html/calendario.php
@@ -2,15 +2,58 @@
         <div class="contenido">
         	<h2>Calendario</h2>
           	<div class="clearfix">
-          		<div class="principales hlpr-mr">
-          		    <p>Grupo A</p>
-          		    <ul>
-          		    	<li>Pais 1</li>
-          		    	<li>Pais 2</li>
-          		    	<li>Pais 3</li>
-          		    	<li>Pais 4</li>
-          		    </ul>
-          		</div>        		
+              <section class="grupo">
+            		<div class="cal-grupo hlpr-mr">
+            		    <p>Grupo A</p>
+            		    <ul>
+            		    	<li>Pais 1</li>
+            		    	<li>Pais 2</li>
+            		    	<li>Pais 3</li>
+            		    	<li>Pais 4</li>
+            		    </ul>
+            		</div>
+                <div class="cal-grupo hlpr-mr">
+                    <p>Grupo b</p>
+                    <ul>
+                      <li>Pais 1</li>
+                      <li>Pais 2</li>
+                      <li>Pais 3</li>
+                      <li>Pais 4</li>
+                    </ul>
+                </div>
+                <div class="cal-grupo hlpr-mr">
+                    <p>Grupo c</p>
+                    <ul>
+                      <li>Pais 1</li>
+                      <li>Pais 2</li>
+                      <li>Pais 3</li>
+                      <li>Pais 4</li>
+                    </ul>
+                </div>
+              </section>
+              <!--Publicidad-->
+              <section class="calendario clearfix">
+                  <div class="cal-fecha hlpr-mr">Jueves 12 de Junio</div>
+                  <div class="cal-fecha hlpr-mr-t">Viernes 13 de Junio</div>
+                  <div class="cal-fecha">Sábado 14 de Junio</div>
+                  <div class="cal-cont">Algún texto adentro</div>
+                  <div class="cal-fecha hlpr-mr">Domingo 15 de Junio</div>
+                  <div class="cal-fecha hlpr-mr-t">Lunes 16 de Junio</div>
+                  <div class="cal-fecha">Martes 17 de Junio</div>
+                  <div class="cal-fecha hlpr-mr">Miércoles 18 de Junio</div>
+                  <div class="cal-fecha hlpr-mr-t">Jueves 19 de Junio</div>
+                  <div class="cal-fecha">Viernes 20 de Junio</div>
+                  <div class="cal-fecha hlpr-mr">Sábado 21 de Junio</div>
+                  <div class="cal-fecha hlpr-mr-t">Domingo 22 de Junio</div>
+                  <div class="cal-fecha">Lunes 23 de Junio</div>
+                  <div class="cal-fecha hlpr-mr">Martes 24 de Junio</div>
+                  <div class="cal-fecha hlpr-mr-t">Miércoles 25 de Junio</div>
+                  <div class="cal-fecha">Jueves 26 de Junio</div>
+                  <div class="cal-fecha hlpr-mr">Viernes 27 de Junio</div>
+                  <div class="cal-fecha hlpr-mr-t">Sábado 28 de Junio</div>
+                  <div class="cal-fecha">Domingo 29 de Junio</div>
+                  <div class="cal-fecha">Lunes 30 de Junio</div>
+              </section>        		
           	</div>
         </div>
         <?php include('includes/footer.php') ?>
