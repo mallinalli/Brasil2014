e931f8d48017fb492a28ffdcd90c5d5f24686cb8
mallinalli <malli.gabarron@gmail.com>
2014-03-21 17:29:38 -0600

    ads

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -73,6 +73,26 @@
             </div>
             <h3 class="foto-footer">Presentado por</h3>
           </div>
+          <div class="aside-banner">
+            <!--/* OpenX Javascript Tag v2.8.11 */-->
+
+            <script type='text/javascript'><!--//<![CDATA[
+               var m3_u = (location.protocol=='https:'?'https://ctrlup.net/openx/www/delivery/ajs.php':'http://ctrlup.net/openx/www/delivery/ajs.php');
+               var m3_r = Math.floor(Math.random()*99999999999);
+               if (!document.MAX_used) document.MAX_used = ',';
+               document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+               document.write ("?zoneid=3");
+               document.write ('&amp;cb=' + m3_r);
+               if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+               document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+               document.write ("&amp;loc=" + escape(window.location));
+               if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+               if (document.context) document.write ("&context=" + escape(document.context));
+               if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+               document.write ("'><\/scr"+"ipt>");
+            //]]>--></script><noscript><a href='http://ctrlup.net/openx/www/delivery/ck.php?n=acb0f673&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://ctrlup.net/openx/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=acb0f673' border='0' alt='' /></a></noscript>
+
+          </div>
         </aside>
       </div>
       <footer class="main-footer">
--- html/includes/header.php
+++ html/includes/header.php
@@ -60,9 +60,25 @@
           </ul>
         </div>
         <div class="top-banner">
-          <!--/* OpenX iFrame Tag v2.8.11 */-->
+         <!--/* OpenX Javascript Tag v2.8.11 */-->
 
-          <iframe id='ad4ff2c3' name='ad4ff2c3' src='http://ctrlup.net/openx/www/delivery/afr.php?zoneid=2&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='1000' height='100'><a href='http://ctrlup.net/openx/www/delivery/ck.php?n=afc4d1ee&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://ctrlup.net/openx/www/delivery/avw.php?zoneid=2&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=afc4d1ee' border='0' alt='' /></a></iframe>
+         <script>
+
+         <!--//<![CDATA[
+            var m3_u = (location.protocol=='https:'?'https://ctrlup.net/openx/www/delivery/ajs.php':'http://ctrlup.net/openx/www/delivery/ajs.php');
+            var m3_r = Math.floor(Math.random()*99999999999);
+            if (!document.MAX_used) document.MAX_used = ',';
+            document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+            document.write ("?zoneid=2");
+            document.write ('&amp;cb=' + m3_r);
+            if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+            document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+            document.write ("&amp;loc=" + escape(window.location));
+            if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+            if (document.context) document.write ("&context=" + escape(document.context));
+            if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+            document.write ("'><\/scr"+"ipt>");
+         //]]>--></script><noscript><a href='http://ctrlup.net/openx/www/delivery/ck.php?n=a9fba726&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://ctrlup.net/openx/www/delivery/avw.php?zoneid=2&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a9fba726' border='0' alt='' /></a></noscript>
 
         </div>
       </header>
