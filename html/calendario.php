1ae5f3340705b4bd10c2ffe1cc5f058ce44390c1
mallinalli <malli.gabarron@gmail.com>
2014-03-21 12:17:24 -0600

    calendario y grupos

--- html/calendario.php
+++ html/calendario.php
@@ -6,52 +6,97 @@
             		<div class="cal-grupo hlpr-mr">
             		    <p>Grupo A</p>
             		    <ul>
-            		    	<li>Pais 1</li>
-            		    	<li>Pais 2</li>
-            		    	<li>Pais 3</li>
-            		    	<li>Pais 4</li>
+            		    	<li><img src="img/bra.jpg">Brasil</li>
+            		    	<li><img src="img/cro.jpg">Croacia</li>
+            		    	<li><img src="img/mex.jpg">México</li>
+            		    	<li><img src="img/cmr.jpg">Camerún</li>
             		    </ul>
             		</div>
                 <div class="cal-grupo hlpr-mr">
-                    <p>Grupo b</p>
+                    <p>Grupo B</p>
                     <ul>
-                      <li>Pais 1</li>
-                      <li>Pais 2</li>
-                      <li>Pais 3</li>
-                      <li>Pais 4</li>
+                      <li><img src="img/esp.jpg">España</li>
+                      <li><img src="img/ned.jpg">Holanda</li>
+                      <li><img src="img/chi.jpg">Chile</li>
+                      <li><img src="img/aus.jpg">Australia</li>
                     </ul>
                 </div>
                 <div class="cal-grupo hlpr-mr">
-                    <p>Grupo c</p>
+                    <p>Grupo C</p>
                     <ul>
-                      <li>Pais 1</li>
-                      <li>Pais 2</li>
-                      <li>Pais 3</li>
-                      <li>Pais 4</li>
+                      <li><img src="img/col.jpg">Colombia</li>
+                      <li><img src="img/gre.jpg">Grecia</li>
+                      <li><img src="img/civ.jpg">Costa de Marfil</li>
+                      <li><img src="img/jpn.jpg">Japón</li>
+                    </ul>
+                </div>
+                <div class="cal-grupo hlpr-mr">
+                    <p>Grupo D</p>
+                    <ul>
+                      <li><img src="img/uru.jpg">Uruguay</li>
+                      <li><img src="img/crc.jpg">Costa Rica</li>
+                      <li><img src="img/eng.jpg">Inglaterra</li>
+                      <li><img src="img/ita.jpg">Italia</li>
+                    </ul>
+                </div>
+                <div class="cal-grupo hlpr-mr">
+                    <p>Grupo E</p>
+                    <ul>
+                      <li><img src="img/sui.jpg">Suiza</li>
+                      <li><img src="img/ecu.jpg">Ecuador</li>
+                      <li><img src="img/fra.jpg">Francia</li>
+                      <li><img src="img/hon.jpg">Honduras</li>
+                    </ul>
+                </div>
+                <div class="cal-grupo hlpr-mr">
+                    <p>Grupo F</p>
+                    <ul>
+                      <li><img src="img/arg.jpg">Argentina</li>
+                      <li><img src="img/bih.jpg">Bosnia</li>
+                      <li><img src="img/irn.jpg">Irán</li>
+                      <li><img src="img/nga.jpg">Nigeria</li>
+                    </ul>
+                </div>
+                <div class="cal-grupo hlpr-mr">
+                    <p>Grupo G</p>
+                    <ul>
+                      <li><img src="img/ger.jpg">Alemania</li>
+                      <li><img src="img/por.jpg">Portugal</li>
+                      <li><img src="img/gha.jpg">Ghana</li>
+                      <li><img src="img/usa.jpg">Estados Unidos</li>
+                    </ul>
+                </div>
+                <div class="cal-grupo hlpr-mr">
+                    <p>Grupo H</p>
+                    <ul>
+                      <li><img src="img/bel.jpg">Bélgica</li>
+                      <li><img src="img/alg.jpg">Argelia</li>
+                      <li><img src="img/rus.jpg">Rusia</li>
+                      <li><img src="img/kor.jpg">Corea del Sur</li>
                     </ul>
                 </div>
               </section>
               <!--Publicidad-->
               <section class="calendario clearfix">
-                  <div class="cal-fecha hlpr-mr">Jueves 12 de Junio</div>
-                  <div class="cal-fecha hlpr-mr-t">Viernes 13 de Junio</div>
-                  <div class="cal-fecha">Sábado 14 de Junio</div>
-                  <div class="cal-fecha hlpr-mr">Domingo 15 de Junio</div>
-                  <div class="cal-fecha hlpr-mr-t">Lunes 16 de Junio</div>
-                  <div class="cal-fecha">Martes 17 de Junio</div>
-                  <div class="cal-fecha hlpr-mr">Miércoles 18 de Junio</div>
-                  <div class="cal-fecha hlpr-mr-t">Jueves 19 de Junio</div>
-                  <div class="cal-fecha">Viernes 20 de Junio</div>
-                  <div class="cal-fecha hlpr-mr">Sábado 21 de Junio</div>
-                  <div class="cal-fecha hlpr-mr-t">Domingo 22 de Junio</div>
-                  <div class="cal-fecha">Lunes 23 de Junio</div>
-                  <div class="cal-fecha hlpr-mr">Martes 24 de Junio</div>
-                  <div class="cal-fecha hlpr-mr-t">Miércoles 25 de Junio</div>
-                  <div class="cal-fecha">Jueves 26 de Junio</div>
-                  <div class="cal-fecha hlpr-mr">Viernes 27 de Junio</div>
-                  <div class="cal-fecha hlpr-mr-t">Sábado 28 de Junio</div>
-                  <div class="cal-fecha">Domingo 29 de Junio</div>
-                  <div class="cal-fecha">Lunes 30 de Junio</div>
+                  <div data-cdate="12" class="cal-fecha hlpr-mr">Jueves 12 de Junio</div>
+                  <div data-cdate="13" class="cal-fecha hlpr-mr-t">Viernes 13 de Junio</div>
+                  <div data-cdate="14" class="cal-fecha">Sábado 14 de Junio</div>
+                  <div data-cdate="15" class="cal-fecha hlpr-mr">Domingo 15 de Junio</div>
+                  <div data-cdate="16" class="cal-fecha hlpr-mr-t">Lunes 16 de Junio</div>
+                  <div data-cdate="17" class="cal-fecha">Martes 17 de Junio</div>
+                  <div data-cdate="18" class="cal-fecha hlpr-mr">Miércoles 18 de Junio</div>
+                  <div data-cdate="19" class="cal-fecha hlpr-mr-t">Jueves 19 de Junio</div>
+                  <div data-cdate="20" class="cal-fecha">Viernes 20 de Junio</div>
+                  <div data-cdate="21" class="cal-fecha hlpr-mr">Sábado 21 de Junio</div>
+                  <div data-cdate="22" class="cal-fecha hlpr-mr-t">Domingo 22 de Junio</div>
+                  <div data-cdate="23" class="cal-fecha">Lunes 23 de Junio</div>
+                  <div data-cdate="24" class="cal-fecha hlpr-mr">Martes 24 de Junio</div>
+                  <div data-cdate="25" class="cal-fecha hlpr-mr-t">Miércoles 25 de Junio</div>
+                  <div data-cdate="26" class="cal-fecha">Jueves 26 de Junio</div>
+                  <div data-cdate="27" class="cal-fecha hlpr-mr">Viernes 27 de Junio</div>
+                  <div data-cdate="28" class="cal-fecha hlpr-mr-t">Sábado 28 de Junio</div>
+                  <div data-cdate="29" class="cal-fecha">Domingo 29 de Junio</div>
+                  <div data-cdate="30" class="cal-fecha">Lunes 30 de Junio</div>
               </section>        		
           	</div>
         </div>
