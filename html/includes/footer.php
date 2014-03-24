40e91acd2e81865c4b4a1fc7422fe15bc2ee540a
mallinalli <malli.gabarron@gmail.com>
2014-03-24 16:13:07 -0600

    aside

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -54,44 +54,46 @@
               <a href="" class="mastracks">Más...</a>
             </div>
           </div>
-          <div class="complemento foto">
-            <h2 class="foto-head">Fotos del día</h2>
-            <div>
-              <div class="jcarousel">
-                <div class="jcarcontrol" id="izq"><span><i class="icon-chevron-left"></i></span></div>
-                <div class="jcarcontrol" id="der"><span><i class="icon-chevron-right"></i></span></div>
-                <div class="jcar-cont">
-                  <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
-                  <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
-                  <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
-                  <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
-                  <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
-                  <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
-                  <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+          <div class="widgets3 clearfix">
+            <div class="complemento foto">
+              <h2 class="foto-head">Fotos del día</h2>
+              <div>
+                <div class="jcarousel">
+                  <div class="jcarcontrol" id="izq"><span><i class="icon-chevron-left"></i></span></div>
+                  <div class="jcarcontrol" id="der"><span><i class="icon-chevron-right"></i></span></div>
+                  <div class="jcar-cont">
+                    <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+                    <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+                    <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+                    <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+                    <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+                    <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+                    <div class="jcaritem"><a class="fancybox-thumbs" data-fancybox-group="thumb" href="img/temp/300x225.gif"><img src="http://placehold.it/100x75" alt="jcaritem"></a></div>
+                  </div>
                 </div>
               </div>
+              <h3 class="foto-footer">Presentado por</h3>
             </div>
-            <h3 class="foto-footer">Presentado por</h3>
-          </div>
-          <div class="aside-banner">
-            <!--/* OpenX Javascript Tag v2.8.11 */-->
+            <div class="aside-banner">
+              <!--/* OpenX Javascript Tag v2.8.11 */-->
 
-            <script type='text/javascript'><!--//<![CDATA[
-               var m3_u = (location.protocol=='https:'?'https://ctrlup.net/openx/www/delivery/ajs.php':'http://ctrlup.net/openx/www/delivery/ajs.php');
-               var m3_r = Math.floor(Math.random()*99999999999);
-               if (!document.MAX_used) document.MAX_used = ',';
-               document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-               document.write ("?zoneid=3");
-               document.write ('&amp;cb=' + m3_r);
-               if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-               document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
-               document.write ("&amp;loc=" + escape(window.location));
-               if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-               if (document.context) document.write ("&context=" + escape(document.context));
-               if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-               document.write ("'><\/scr"+"ipt>");
-            //]]>--></script><noscript><a href='http://ctrlup.net/openx/www/delivery/ck.php?n=acb0f673&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://ctrlup.net/openx/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=acb0f673' border='0' alt='' /></a></noscript>
+              <script type='text/javascript'><!--//<![CDATA[
+                 var m3_u = (location.protocol=='https:'?'https://ctrlup.net/openx/www/delivery/ajs.php':'http://ctrlup.net/openx/www/delivery/ajs.php');
+                 var m3_r = Math.floor(Math.random()*99999999999);
+                 if (!document.MAX_used) document.MAX_used = ',';
+                 document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+                 document.write ("?zoneid=3");
+                 document.write ('&amp;cb=' + m3_r);
+                 if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+                 document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+                 document.write ("&amp;loc=" + escape(window.location));
+                 if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+                 if (document.context) document.write ("&context=" + escape(document.context));
+                 if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+                 document.write ("'><\/scr"+"ipt>");
+              //]]>--></script><noscript><a href='http://ctrlup.net/openx/www/delivery/ck.php?n=acb0f673&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://ctrlup.net/openx/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=acb0f673' border='0' alt='' /></a></noscript>
 
+            </div>
           </div>
         </aside>
       </div>
