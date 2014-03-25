d1665ff71756d356a7387f3610783a0dfdd44fb0
jecr <sinnerei@gmail.com>
2014-03-25 13:45:10 -0600

    Ajuste noticias, ahora expandibles¿?

--- html/noticias.php
+++ html/noticias.php
@@ -1,9 +1,9 @@
         <?php include('includes/header.php') ?>
         <div class="contenido">
           	<h2>Noticias</h2>
-         	<div class="clearfix">
-	        	<h3 class="day">13 de marzo</h3>
-	        	<div class="clearfix">
+         	<div class="day-cont">
+	        	<h3 class="day"><i class="icon-caret-right"></i>&nbsp;13 de Marzo</h3>
+	        	<div class="day-block">
 			        <div class="nota-s clearfix">
 			        	<a href="nota.php"></a>
 				            <img src="http://placehold.it/110x78" alt="#">
@@ -38,9 +38,9 @@
 			        </div>
 			    </div>
 	     	</div>
-	     	<div class="clearfix">
-		        <h3 class="day">12 de marzo</h3>
-		        <div class="clearfix">
+	     	<div class="day-cont">
+		        <h3 class="day"><i class="icon-caret-right"></i>&nbsp;12 de Marzo</h3>
+		        <div class="day-block">
 			        <div class="nota-s clearfix">
 			        	<a href="nota.php"></a>
 				            <img src="http://placehold.it/110x78" alt="#">
