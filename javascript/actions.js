e9c4cc940ac8b86ef176a91a85192eee7eef9b8c
jecr <sinnerei@gmail.com>
2014-05-16 12:46:56 -0500

    banners

--- javascript/actions.js
+++ javascript/actions.js
@@ -31,8 +31,6 @@ $(document).ready(function(){
   $('.day-cont .day-block').first().slideToggle();
   $('.day-cont .day-block').first().closest('.day-cont').find('i').attr('class','icon-caret-down');
 
-  /*Galería de infografías*/
-  $('.info').find('.infogs').css('background','url('+$(this).find('img').attr('src')+') no-repeat center center' ).html('');
 });
 
 
